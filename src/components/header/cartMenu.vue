ec0c2c78e078fe261d097f36c10646fe9834071c
Abdul-aziz-shuvo <shuvoahmedkhan202@gmail.com>
2021-03-24 02:17:37 +0800

    cart-menu component

---
+++ src/components/header/cartMenu.vue
@@ -0,0 +1,31 @@
+<template>
+         <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" style="right:0;left:auto;min-width:300px">
+           <div class="row m-0">
+             <div class="col-5 ml-2 border-bottom">
+               <p class="m-0">Product Title</p>
+               <p class="m-0">1 x 10 </p>
+             </div>
+
+              <div class="col-6 border-bottom" style="text-align:end"> 
+                <button type="" class="badge bg-secondary mr-2">remove</button>
+             </div>
+
+             <div class="col-6">
+               Total Price : 
+             </div>
+
+              <div class="col-6" style="text-align:end">
+              $123
+             </div>
+           </div>
+           
+        </ul>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,26 +17,15 @@
         </li>
       </ul>
     </div>
-
+      <!-- cart menu -->
       <div class="dropdown">
         <button class="btn btn-light dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
            Cart
         </button>
-        <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1" style="right:0;left:auto;min-width:300px">
-           
-           <div class="row m-0">
-             <div class="col-5 ml-2 border-bottom">
-               <p class="m-0">Product Title</p>
-               <p class="m-0">1 x 10 </p>
-             </div>
-
-              <div class="col-6 border-bottom" style="text-align:end"> 
-                <button type="" class="badge bg-secondary mr-2">remove</button>
-             </div>
-           </div>
-           
-        </ul>
+         <!-- cart men component -->
+        <cart-menu />  
       </div>
+      <!-- cart menu -->
 
   </div>
 </nav>
@@ -45,8 +34,13 @@
 </template>
 
 <script>
+
+import CartMenu from './cartMenu.vue'
     export default {
-        name: 'Nav'
+        name: 'Nav',
+        components:{
+          CartMenu
+        }
     }
 </script>
 
